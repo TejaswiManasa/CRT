080d9336b4b34696783615a315d5a8cdf0cb5bcb
TejaswiManasa <132214337+TejaswiManasa@users.noreply.github.com>
2024-07-02 20:14:06 +0530

    XOR apps.

---
+++ xor.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+//	int cal(int x){         //range XOR
+//	if(x%4==0)
+//	return x;
+//	else if(x%4==1)
+//	return 1;
+//	else if(x%4==2)
+//	return x+1;
+//	else
+//	return 0; 
+//	}
+int main(){
+//	int x=5,p=0,i;
+//	for(i=x;i>0;i--){
+//		p=p^i;
+//	}
+//	printf("%d",p);      //O(n)
+	
+//	int x; 
+//	scanf("%d",&x);
+//	if(x%4==0)
+//	printf("%d",x);
+//	else if(x%4==1)
+//	printf("1");
+//	else if(x%4==2)
+//	printf("%d",x+1);
+//	else
+//	printf("0");               //O(1)
+
+
+//	int x,y,p;                     //range XOR
+//	scanf("%d %d",&x,&y);
+//	p=cal(y)^cal(x-1);
+//	printf("%d",p);
+
+
+	
+	return 0;
+}
