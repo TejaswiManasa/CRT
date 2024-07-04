2efef11480a5ce8cf0f023a3607eadc395fa0e59
TejaswiManasa <132214337+TejaswiManasa@users.noreply.github.com>
2024-07-04 19:57:01 +0530

    practice code snippets

---
+++ 10dmcq.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+//int sum(int x,int y){			//actual int sum
+//int sum(x,y){					//garbage value
+//int sum(float x,float y){ 	//%d -15; %f -0- returning int value
+//float sum(float x,float y){	//returns float value with %f 
+//	return x+y;
+//}
+
+//int fun(int x){    			//ascii of A
+//int fun(char x){				//acsii of A
+////	return x;
+//	return (char)x;				//character A if %c else 65 if %d
+//}
+
+int main(){
+		
+//	float a=10.3,b=4.8;
+//	printf("%d\n",sum(a,b));
+//	printf("%f",sum(a,b));
+
+//	char c='A';
+//	printf("%d",fun(c));
+//	printf("%c",fun(c));
+  			
+  	
+	  		
+	return 0;
+}
